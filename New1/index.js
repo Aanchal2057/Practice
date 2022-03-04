3ca334f3b0ce64f99af6887840d86d1382e47a6d
Aanchal2057 <61676170+Aanchal2057@users.noreply.github.com>
2022-03-04 09:52:02 +0545

    Add files via upload

---
+++ New1/index.js
@@ -0,0 +1,84 @@
+$(function(){
+
+    
+    //for changing the length of search input field of the navbar
+  
+   $(".d-flex").find('input').focus(function(){
+       
+       $('.disappear').stop().hide();
+       
+       $(this).stop().css({"outline":'none'}).animate({'width':'700px',}).fadeIn();
+       
+   });
+
+  
+   $("input[type='text']").stop().blur(function(){
+       
+       $('.disappear').stop().show();
+      
+       $(this).stop().animate({'width':"50%"}).fadeIn();
+       $(".bi-search").delay(500).fadeIn()
+   });
+   //for showing products on hovering nav content
+   $('.navbar-nav').find('.show-hover').mouseenter(function(){
+      
+    $('#hover-nav-element').stop().empty();
+    var text = $('<h1>').text($(this).text())
+    $(this).stop().css({'color':'black'});
+     $('#hover-nav-element').stop()
+                             .prepend(text).
+                             append("<img src='./img/product/ee.jpg' alt= height='200px' width='200px'>").
+                             append("<h4>earings</h4>").
+                             css({'display':'block','top':$(this)[0].offsetHeight+15,"left":$(this)[0].offsetLeft,'z-index':10000000})
+                             $('#hover-nav-element').stop().css({'display':'block'})
+                         })
+    $('.show-hover').mouseleave(function(){
+     
+     $(this).stop().css({'font-size':'16px'});
+     $('#hover-nav-element').stop().hide();
+    });
+     $('#hover-nav-element').mouseenter(function(){
+     $(this).stop().show();
+     })
+     $('#hover-nav-element').mouseleave(function(){
+     $(this).stop().hide();
+    });
+        
+ // for showimg create Account and log in container
+ $('.bi-person').mouseenter(function(){
+     $('#hover-nav-element').stop().empty();
+     $('#hover-nav-element').stop()
+                             .prepend("<h4>Account</h4>")
+                             .append("<button type='button'>Log in</button>")
+                             .append("<button type='button'>Create Account</button>")
+                             .css({'display':'block','top':$(this)[0].offsetHeight,"left":$(this)[0].offsetLeft-300,'height':'auto','width':'25%','text-align':'center','z-index':1000000});
+         $('#hover-nav-element').stop().find('button')
+                                         .css({'background-color':'#ADD8E6','width':'90%','margin':'5px','border':'0px','padding':'5px','font-weight':"bold"})
+ })
+ $('.bi-person').mouseleave(function(){
+     $('#hover-nav-element').stop().hide();
+ });
+
+   //for highligting para 
+   $("p").css("background-color","rgba(173,216,230,0.7)");
+   $("input[type='text'] ").css("background-color","rgba(173,216,230,0.7)");
+   //for fade-in and out images
+   $( ".fe-box" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+  });
+$( ".pro" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+  });
+$(function(){
+        $("#move").hide(1000);
+        $("#move").show(1000);
+});
+$('.banner-box').click(function(){
+    $('#move').hide(1000);
+    $('#move').show(2000);
+});
+
+
+});
